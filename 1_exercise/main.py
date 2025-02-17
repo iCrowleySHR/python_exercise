9b2dcb6e8823993349bbac2f0740046ab1e933dd
iCrowleySHR <gustavogualda6@gmail.com>
2025-02-17 10:49:47 -0300

    feat: first exercise

---
+++ 1_exercise/main.py
@@ -0,0 +1,23 @@
+from src.functions import sum, subtract, multiplication, division
+
+def main():
+    print('Digite o primeiro valor')
+    n1 = float(input())
+
+    print('Digite o segundo valor')
+    n2 = float(input())
+
+    resultado = show_results(n1, n2)
+    print(resultado)
+
+def show_results(n1, n2):
+    result = (
+        f"Soma: {sum(n1, n2)}\n"
+        f"Subtração: {subtract(n1, n2)}\n"
+        f"Vezes: {multiplication(n1, n2)}\n"
+        f"Divisão: {division(n1, n2)}\n"
+    )
+    return result
+
+if __name__ == "__main__":
+    main()
---
+++ 1_exercise/src/functions.py
@@ -0,0 +1,14 @@
+def sum(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiplication(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    if n2 != 0:
+        return n1 / n2
+    else:
+        return "Erro: Divisão por zero"
