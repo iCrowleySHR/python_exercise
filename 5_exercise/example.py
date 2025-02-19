1e3c52f33fb9baa8594aa7417ed60c2f32cd447d
iCrowleySHR <gustavogualda6@gmail.com>
2025-02-19 11:55:09 -0300

    feat: create 5_exercise

---
+++ 5_exercise/example.py
@@ -0,0 +1,14 @@
+minha_lista = [1, 2, 3]
+minha_lista.append(4) # Adicina sempre no final do array
+print(minha_lista)
+# Saída: [1, 2, 3, 4]
+
+minha_lista = [1, 2, 3]
+minha_lista.insert(1, 4)  # O número 4 será inserido na posição 1
+print(minha_lista)
+# Saída: [1, 4, 2, 3]
+
+minha_lista = [1, 2, 3]
+minha_lista.extend([4, 5]) # Adiciona varios valores no final do array
+print(minha_lista)
+# Saída: [1, 2, 3, 4, 5]
---
+++ 5_exercise/main.py
@@ -0,0 +1,9 @@
+value = []
+
+for x in range(3):
+   value.append(x)
+   
+for y in range(3):
+    print(value)
+    
+
